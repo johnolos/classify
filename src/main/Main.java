195e69c55e1e04b60a7bb1957f1c2980beb68d17
magnumog <magnumog@stud.ntnu.no>
2014-12-06 12:00:29 -0800

    forgot to add main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,13 @@
 package main;
 
 import readTestData.ReadFile;
+import statistics.StartProbability;
 
 public class Main {
 
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		ReadFile read = new ReadFile("C:/Users/Magnus Mogstad/Dropbox/CS273 Data and knowledge bases/test.txt");
+		StartProbability prob = new StartProbability(read.getWordList(), read.getEntityList());
 	}
-
 }
